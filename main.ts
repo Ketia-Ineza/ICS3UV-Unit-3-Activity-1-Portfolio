48b414422575b957ac4314f35d1c50fd27cebdac
default <ketia.ineza@stu.ocsb.ca>
2025-11-24 17:06:12 +0000

    Added constants NUMBER1,NUMBER2,NUMBER3

---
+++ main.ts
@@ -0,0 +1,32 @@
+/**
+ * @author Ketia
+ * @version 1.0.0
+ * @date 2025-11-24
+ * @fileoverview This program adds 3 constants together.
+ */
+
+// initialize numbers
+const NUMBER1: number = 10;
+const NUMBER2: number = -20;
+const NUMBER3: number = 85;
+
+// INPUT – none
+
+// PROCESS
+// calculate the sum
+let answer: number = NUMBER1 + NUMBER2 + NUMBER3;
+
+// OUTPUT
+// display the result
+console.log(
+  "The sum of " +
+  NUMBER1 +
+  " + " +
+  NUMBER2 +
+  " + " +
+  NUMBER3 +
+  " is: " +
+  answer
+);
+
+console.log("\nDone.");
